8af77b4b1a01571f95ec22a9f9b32f9a35d5ea86
Animesh Samal <animeshsamal1995@gmail.com>
2022-03-29 21:11:37 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express=require("express");
 const app=express();
 
@@ -7,4 +8,15 @@ app.get('/',(req,res)=>{
 
 app.listen(7000,()=>{
     console.log(`aerver is running`);
+=======
+const express = require('express');
+const app = express();
+
+app.get('/' ,(req,res) =>{
+    res.send(`hello world from the server`);
+});
+
+app.listen(7000,()=>{
+    console.log(`server is runnimg at port no 7000`);
+>>>>>>> Stashed changes
 })
